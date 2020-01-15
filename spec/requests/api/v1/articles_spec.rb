60bb588edc0f6165d1332036fba7706d954d776c
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-01-15 16:07:34 +0100

    Adds specs for index action

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -1,33 +1,48 @@
-RSpec.describe 'GET /api/v1/articles/:id', type: :request do
+RSpec.describe Articles, type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
   let!(:article) do
     create(:article,
           title: 'Breaking News',
           body: 'Some breaking content')
   end
-
-  describe 'Successfully' do
-    before do
-      get "/api/v1/articles/#{article.id}", headers: headers
+  
+  describe 'GET /api/v1/articles/:id' do
+    describe 'Successfully' do
+      before do
+        get "/api/v1/articles/#{article.id}", headers: headers
+      end
+  
+      it 'returns a 200 response status' do
+        expect(response).to have_http_status 200
+      end
+  
+      it 'returns article title' do
+        expect(response_json["title"]).to eq "Breaking News"
+      end
     end
-
-    it 'returns a 200 response status' do
-      expect(response).to have_http_status 200
-    end
-
-    it 'returns article title' do
-      expect(response_json["title"]).to eq "Breaking News"
+  
+    describe 'With invalid :id' do
+      before do
+        get "/api/v1/articles/10000", headers: headers
+      end
+  
+      it 'returns error if ID does not exist' do
+        expect(response_json["error"]).to eq "ID does not exist"
+      end
     end
   end
 
-  describe 'With invalid :id' do
+  describe 'GET /api/v1/articles' do
     before do
-      get "/api/v1/articles/10000", headers: headers
+      get '/api/v1/articles', headers: headers
     end
 
-    it 'returns error if ID does not exist' do
-      expect(response_json["error"]).to eq "ID does not exist"
+    it 'return a 200 response status' do
+      expect(response).to have_http_status 200
     end
 
+    it 'returns 4 articles' do
+      expect(response_json['articles']).to eq 4
+    end
   end
 end
