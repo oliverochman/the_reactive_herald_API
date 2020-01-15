68d94ee850937157936ba24e57dca039b02ae203
Eevan <eevan.redon@gmail.com>
2020-01-15 16:21:55 +0100

    creating articles index route, def index in articles controller, render json

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,13 +1,18 @@
 class Api::V1::ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
+  def index
+    articles = Article.all
+    render json: articles
+  end
+
   def show
     article = Article.find(params[:id])
     render json: article, serializer: Articles::ShowSerializer
   end
 
   private
-  
+
   def not_found
     render json: { error: "ID does not exist"}
   end
